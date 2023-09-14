58fca41fef6931325eb29ed9b7bdee6074089b05
Benttyman <jesustapiamartin@gmail.com>
2023-09-14 09:43:07 -0300

    Creación de los 4 métodos, con las 4 operaciones básicas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,4 +2,36 @@ package org.example;
 
 public class Main {
 
+    //========== MAIN ==========
+    public static void main(String[] args) {
+
+    }
+
+
+
+    //========== SUMA ==========
+    public static double suma(int num1, int num2){
+        return num1 + num2;
+    }
+
+
+
+    //========== RESTA ==========
+    public static double resta(int num1, int num2){
+        return num1 - num2;
+    }
+
+
+
+    //========== MULTIPLICACIÓN ==========
+    public static double multiplicacion(int num1, int num2){
+        return num1 * num2;
+    }
+
+
+
+    //========== DIVISIÓN ==========
+    public static double division(int num1, int num2){
+        return num1 / num2;
+    }
 }
