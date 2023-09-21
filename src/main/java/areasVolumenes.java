02c183bf2ce6413bd05d71e7ea95579535238cb3
JesusTapiaMartin <jesustapiamartin@gmail.com>
2023-09-20 22:30:03 -0300

    Estructuración del código

--- src/main/java/areasVolumenes.java
+++ src/main/java/areasVolumenes.java
@@ -1,8 +1,6 @@
-package org.example;
-
 public class areasVolumenes {
 
-    // Area y volumen del cuadrado
+
     //============================ CUADRADO ============================
     public static double perimetroCuadrado (double lado){
         return 4*lado;
@@ -11,6 +9,8 @@ public class areasVolumenes {
     public static double areaCuadrado (double lado){
         return lado*lado;
     }
+
+
 
     //============================ RECTANGULO ============================
     public static double perimetroRectangulo (double lado1, double lado2){
@@ -21,6 +21,8 @@ public class areasVolumenes {
         return lado1*lado2;
     }
 
+
+
     //============================ CIRCULO ============================
     public static double perimetroCirculo (double radio){
         return 2 * Math.PI * radio;
@@ -29,6 +31,8 @@ public class areasVolumenes {
     public static double areaCirculo (double radio){
         return Math.PI * Math.pow(radio, 2);
     }
+
+
 
     //============================ ESFERA ============================
     public static double superficieEsfera (double radio){
@@ -39,6 +43,8 @@ public class areasVolumenes {
         return (4/3) * Math.PI * Math.pow(radio, 3);
     }
 
+
+
     //============================ CUBO ============================
     public static double superficieCubo (double lado){
         return 6 * Math.pow(lado, 2);
@@ -47,12 +53,13 @@ public class areasVolumenes {
         return Math.pow(lado,3);
     }
 
+
+
     //============================ CONO ============================
     public static double superficieCono (double radio, double altura){
         //nse como se hace dou
         return radio;
     }
-
     public static double volumenCono (double radio, double altura){
         return (1/3) * Math.PI * Math.pow(radio, 2) * altura;
     }
